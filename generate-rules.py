8f33d7bedb66eb233200061ea366a634b71090fb
Sherine Awad <sherine.awad@gmail.com>
2015-03-18 11:51:50 -0400

    update generate rules

--- generate-rules.py
+++ generate-rules.py
@@ -8,59 +8,59 @@ sup_repeats = 0
 sup_cov = 0
 
 qc = set ()
-abundance = set () 
-repeats = set ()
-coverage = set ()
+abundance = set() 
+repeats = set()
+coverage = set()
+qc_abun = set() 
+qc_cov = set()
+qc_repeats = set()
+abun_cov = set()
+abun_repeats = set()
+cov_repeats = set()
 
-sup_qc_abun = 0
-sup_qc_cov = 0
-sup_qc_repeats = 0
-sup_abun_cov = 0
-sup_cov_repeats = 0
-sup_abun_repeats = 0
+qc_abun_repeat = set()
+qc_abun_cov = set ()
+abun_cov_repeat = set ()
+qc_cov_repeat = set ()
 
 
 for line in open ('qc.low'): 
        name = line.split('-')
        name = name[0]
-       sup_qc += 1
-       qc.add(name[0]) 
+       qc.add(name) 
 
 for line in open ('abundance.low'): 
       name = line.split('-') 
       name = name[0]
-      sup_abun += 1
-      abundance.add(name[0])
-      if name[0] in qc: 
-           sup_qc_abun +=1 
-       
+      abundance.add(name)
 
 for line in open ('coverage.low'):
-      sup_cov += 1
       coverage.add(line)
-      if line in qc: 
-          sup_qc_cov +=1 
-      if line in abundance :
-         sup_abun_cov += 1
  
 for line in open ('repeats.low'): 
-      sup_repeats += 1     
       repeats.add(line)
-      if line in qc: 
-          sup_qc_repeats +=1
-      if line in abundance: 
-         sup_abun_repeats +=1 
-      if line in coverage :
-         sup_cov_repeats += 1      
 
-print 'Support of low quality reads is', sup_qc ,'\nSupport of low abundance reads is ', sup_abun, '\n'
+qc_abun = qc.intersection(abundance) 
+qc_cov = qc.intersection(coverage) 
+qc_repeats = qc.intersection(repeats) 
+abun_cov = abundance.intersection (coverage) 
+abun_repeats = abundance.intersection(repeats)
+cov_repeats = coverage.intersection(repeats)
 
-print 'Support of low quality and low abundance reads is ',  sup_qc_abun
-        
-print 'Support of low quality and low coverage reads is ',  sup_qc_cov 
+qc_abun_repeat = qc_abun.intersection(repeats) 
+qc_abun_cov = qc_abun.intersection(coverage) 
+abun_cov_repeat = abun_cov.intersection(repeats) 
+qc_cov_repeat = qc_cov.intersection(repeats) 
 
-print 'Support of low abundnce and low coverage  is ',  sup_abun_cov
 
-print 'Support of low abundance reads and repeats is ',  sup_abun_repeats
+print 'Support of low quality and abundance is', len(qc_abun) 
+print 'Support of low quality and coverage is', len(qc_cov)
+print 'Support of low quality and repeats is', len(qc_repeats) 
+print 'Support of low abundance and coverage is ', len(abun_cov) 
+print 'Support of low abundance and repeats is', len(abun_repeats) 
+print 'Support of low coverage and repeats is', len(cov_repeats)
 
-print 'Support of low coverage and repeats is ',  sup_cov_repeats
+print 'Support of low quality, abundance, and repeats is ', len(qc_abun_repeat) 
+print 'Support of low quality, abundance, and coverage is ', len(qc_abun_cov) 
+print 'Support of low abundance, coverage, and repeats is', len(abun_cov_repeat) 
+print 'Support of low quality, coverage, and repeats is',  len(qc_cov_repeat) 
